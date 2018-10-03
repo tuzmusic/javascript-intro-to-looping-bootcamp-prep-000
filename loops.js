a7898da42715a1f2af77e6a3e6da6d8e93d08d3e
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-03 15:58:51 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -12,8 +12,12 @@ function whileLoop(n) {
   return 'done'
 }
 
+function incrementVariable() {
+  i = i + 1
+}
+
 function doWhileLoop(array) {
   do {
     array.pop()
-  } while (array.length > 0)
+  } while (array.length > 0 && incrementVariable())
 }
