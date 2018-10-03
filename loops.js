72375354787b70f6e92afbbc1984fb27cd62cee8
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-03 15:47:03 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
 function forLoop(array) {
-  for (var i=0; i<24; i++) {
+  for (i=0; i<24; i++) {
     array.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`)
     console.log(array)
     return array
